a68493766d67ca11ef762a707029f346696aadaa
Ben Krieger <me@benkrieger.dev>
2025-10-03 00:40:42 -0400

    Update actual server with build info and production settings

--- pkgs/by-name/ac/actual-server/package.nix
+++ pkgs/by-name/ac/actual-server/package.nix
@@ -8,8 +8,7 @@
   python3,
   yarn-berry_4,
   nixosTests,
-}:
-let
+}: let
   yarn-berry = yarn-berry_4;
   version = "25.10.0";
   src = fetchFromGitHub {
@@ -28,107 +27,109 @@ let
     rev = "b7f6b55536b02690ce08c4ae4523607de132e4f6";
     hash = "sha256-Sa5lwN4lilEdh3geUeBb+ECOyrdGRcMP6LruvQ9aaMw=";
   };
-
 in
-stdenv.mkDerivation (finalAttrs: {
-  srcs = [
-    src
-    translations
-  ];
-  sourceRoot = "${src.name}/";
-
-  nativeBuildInputs = [
-    yarn-berry
-    nodejs_22
-    yarn-berry.yarnBerryConfigHook
-    (python3.withPackages (ps: [ ps.setuptools ])) # Used by node-gyp
-    makeWrapper
-  ];
-  env = {
-    ELECTRON_SKIP_BINARY_DOWNLOAD = "1";
-    NODE_JQ_SKIP_INSTALL_BINARY = "true";
-  };
-
-  postPatch = ''
-    ln -sv ../../../${translations.name} ./packages/desktop-client/locale
-
-    patchShebangs --build ./bin ./packages/*/bin
-
-    # Patch all references to `git` to a no-op `true`. This neuter automatic
-    # translation update.
-    substituteInPlace bin/package-browser \
-      --replace-fail "git" "true"
-
-    # Allow `remove-untranslated-languages` to do its job.
-    chmod -R u+w ./packages/desktop-client/locale
-
-    # Disable the postinstall script for `protoc-gen-js` because it tries to
-    # use network in buildPhase. It's just used as a dev tool and the generated
-    # protobuf code is committed in the repository.
-    cat <<< $(${lib.getExe jq} '.dependenciesMeta."protoc-gen-js".built = false' ./package.json) > ./package.json
-  '';
-
-  buildPhase = ''
-    runHook preBuild
-
-    export HOME=$(mktemp -d)
-
-    yarn build:server
-    yarn workspace @actual-app/sync-server build
-
-    runHook postBuild
-  '';
-
-  missingHashes = ./missing-hashes.json;
-  offlineCache = yarn-berry.fetchYarnBerryDeps {
-    inherit (finalAttrs) src missingHashes;
-    hash = "sha256-yvlVZPsv9yW1sQp/wscor7nMnws376pERd1rGoJQQyU=";
-  };
-
-  pname = "actual-server";
-  inherit version src;
-
-  installPhase = ''
-    runHook preInstall
-
-    mkdir -p $out/{bin,lib,lib/actual/packages/sync-server,lib/actual/packages/desktop-client}
-    cp -r ./packages/sync-server/build/{app.js,src,migrations,bin} $out/lib/actual/packages/sync-server
-    # sync-server uses package.json to determine path to web ui.
-    cp ./packages/desktop-client/package.json $out/lib/actual/packages/desktop-client
-    cp -r packages/desktop-client/build $out/lib/actual/packages/desktop-client/build
-
-    # Re-create node_modules/ to contain just production packages required for
-    # sync-server itself, using existing offline cache. This will also now build
-    # binaries.
-    export HOME=$(mktemp -d)
-
-    yarn workspaces focus @actual-app/sync-server --production
-    rm -r node_modules/.bin
-    cp -r ./node_modules $out/lib/actual/
-
-    makeWrapper ${lib.getExe nodejs_22} "$out/bin/actual-server" \
-      --add-flags "$out/lib/actual/packages/sync-server/bin/actual-server.js" \
-      --set NODE_PATH "$out/actual/lib/node_modules"
-
-    runHook postInstall
-  '';
-
-  passthru = {
-    inherit (finalAttrs) offlineCache;
-    tests = nixosTests.actual;
-  };
-
-  meta = {
-    changelog = "https://actualbudget.org/docs/releases";
-    description = "Super fast privacy-focused app for managing your finances";
-    homepage = "https://actualbudget.org/";
-    mainProgram = "actual-server";
-    license = lib.licenses.mit;
-    # https://github.com/NixOS/nixpkgs/issues/403846
-    broken = stdenv.hostPlatform.isDarwin;
-    maintainers = [
-      lib.maintainers.oddlama
-      lib.maintainers.patrickdag
+  stdenv.mkDerivation (finalAttrs: {
+    srcs = [
+      src
+      translations
     ];
-  };
-})
+    sourceRoot = "${src.name}/";
+
+    nativeBuildInputs = [
+      yarn-berry
+      nodejs_22
+      yarn-berry.yarnBerryConfigHook
+      (python3.withPackages (ps: [ps.setuptools])) # Used by node-gyp
+      makeWrapper
+    ];
+    env = {
+      ELECTRON_SKIP_BINARY_DOWNLOAD = "1";
+      NODE_JQ_SKIP_INSTALL_BINARY = "true";
+    };
+
+    postPatch = ''
+      ln -sv ../../../${translations.name} ./packages/desktop-client/locale
+
+      patchShebangs --build ./bin ./packages/*/bin
+
+      # Patch all references to `git` to a no-op `true`. This neuter automatic
+      # translation update.
+      substituteInPlace bin/package-browser \
+        --replace-fail "git" "true"
+
+      # Allow `remove-untranslated-languages` to do its job.
+      chmod -R u+w ./packages/desktop-client/locale
+
+      # Disable the postinstall script for `protoc-gen-js` because it tries to
+      # use network in buildPhase. It's just used as a dev tool and the generated
+      # protobuf code is committed in the repository.
+      cat <<< $(${lib.getExe jq} '.dependenciesMeta."protoc-gen-js".built = false' ./package.json) > ./package.json
+    '';
+
+    buildPhase = ''
+      runHook preBuild
+
+      export HOME=$(mktemp -d)
+
+      yarn build:server
+      yarn workspace @actual-app/sync-server build
+
+      runHook postBuild
+    '';
+
+    missingHashes = ./missing-hashes.json;
+    offlineCache = yarn-berry.fetchYarnBerryDeps {
+      inherit (finalAttrs) src missingHashes;
+      hash = "sha256-yvlVZPsv9yW1sQp/wscor7nMnws376pERd1rGoJQQyU=";
+    };
+
+    pname = "actual-server";
+    inherit version src;
+
+    installPhase = ''
+      runHook preInstall
+
+      mkdir -p $out/{bin,lib,lib/actual/packages/sync-server,lib/actual/packages/desktop-client}
+      cp -r ./packages/sync-server/build/{app.js,src,migrations,bin} $out/lib/actual/packages/sync-server
+      # sync-server uses package.json to determine version info
+      cp ./packages/sync-server/package.json $out/lib/actual/packages/sync-server
+      # sync-server uses package.json to determine path to web ui.
+      cp ./packages/desktop-client/package.json $out/lib/actual/packages/desktop-client
+      cp -r packages/desktop-client/build $out/lib/actual/packages/desktop-client/build
+
+      # Re-create node_modules/ to contain just production packages required for
+      # sync-server itself, using existing offline cache. This will also now build
+      # binaries.
+      export HOME=$(mktemp -d)
+
+      yarn workspaces focus @actual-app/sync-server --production
+      rm -r node_modules/.bin
+      cp -r ./node_modules $out/lib/actual/
+
+      makeWrapper ${lib.getExe nodejs_22} "$out/bin/actual-server" \
+        --add-flags "$out/lib/actual/packages/sync-server/bin/actual-server.js" \
+        --set NODE_ENV production \
+        --set NODE_PATH "$out/actual/lib/node_modules"
+
+      runHook postInstall
+    '';
+
+    passthru = {
+      inherit (finalAttrs) offlineCache;
+      tests = nixosTests.actual;
+    };
+
+    meta = {
+      changelog = "https://actualbudget.org/docs/releases";
+      description = "Super fast privacy-focused app for managing your finances";
+      homepage = "https://actualbudget.org/";
+      mainProgram = "actual-server";
+      license = lib.licenses.mit;
+      # https://github.com/NixOS/nixpkgs/issues/403846
+      broken = stdenv.hostPlatform.isDarwin;
+      maintainers = [
+        lib.maintainers.oddlama
+        lib.maintainers.patrickdag
+      ];
+    };
+  })
